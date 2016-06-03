9ec5a9ff98286d542ff1cf6e3dc49f821e3c03b2
brennanwiner <brennan.winer@gmail.com>
2016-06-03 10:53:55 -0400

    added check for collision with other blocks

--- TetrisController.java
+++ TetrisController.java
@@ -4,6 +4,7 @@ Keeps the game running.
 public class TetrisController extends Application{
   private Board board;
   private TetrisView view;
+  private long time;
   
   public TetrisController(){
     board = new Board();
@@ -15,7 +16,25 @@ public class TetrisController extends Application{
     view = tw;
   }
   
-  public void runGame(){
-    
+  public boolean isFinalPosition(Block b){
+  for(int i = 0; i < 5; i++){
+  if((b[i][4].isEmpty() == false){
+  if(board[b.getColumn(not implemented)][b.getRow(not implemented)+1].isEmpty==false){
+  return true;  
+  } } }
+  return false;   
+  }
+  
+  public void runGame(Block b){
+  time = System.nanoTime();
+  while(System.nanoTime()-time < 500000000)
+  //code
+  }
+  if(b.isFinalPosition()){
+  runGame(new Block();)  
+  }
+  else{
+  runGame(b);
+  }
   }
 }
