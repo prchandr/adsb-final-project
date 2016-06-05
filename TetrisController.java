95e9eab3d04a047f77e5f421734a67af3cbb884a
prchandr <happyprems@gmail.com>
2016-06-05 00:35:58 -0400

    Updated TetrisController.java
    Added Block control and UserActions

--- TetrisController.java
+++ TetrisController.java
@@ -3,14 +3,15 @@ Keeps the game running.
 */
 import javafx.animation.AnimationTimer;
 
-
 public class TetrisController{
   private Board board;
   private TetrisView view;
-  private long time;
   private int score;
   private int rowsCleared;
   private int level;
+  private ArrayList<Block> blockList;
+  private int numBlocks;
+  private int fallSpeed;
   
   public TetrisController(){
     board = new Board();
@@ -22,16 +23,14 @@ public class TetrisController{
     view = tw;
   }
   
-  public boolean isFinalPosition(Block b){
-    for(int i = 0; i < 5; i++){
-      if((b[i][4].isEmpty() == false){
-        if(board[b.getColumn(not implemented)][b.getRow(not implemented)+1].isEmpty==false){
-          return true;  
-        } 
-      }
-    }
-    return false;   
+  public Block[] getBlockList(){
+    return blockList;
   }
+  
+  public int getScore(){
+    return score;
+  }
+  
   
   public void runGame(){
     final long startTime = System.nanoTime();
@@ -39,17 +38,43 @@ public class TetrisController{
     score = 0;
     rowsCleared = 0;
     level = 1;
+    blockList = new ArrayList<Block>();
+    numBlocks = 4;
+    fallSpeed = 500;
+    
     
     if(board.canPlay()){
        new AnimationTimer(){
          public void handle(long currentTime){
+           //Gets action from viewer
            UserAction action = tw.getUserAction();
-           if(ticks%500==0||action==DOWN){
-             //Move block down one height because of gravity
+           
+           //Updates the number of blocks visible to the player
+           numBlocks = 4-level/5;
+           if(numBlocks<1){
+             numBlocks = 1;
+           }
+           
+           for(int i = blockList.size(); i<numBlocks; i++){
+             blockList.add(BlockFactory.createPentrisBlock(board));
+           }
+           Block mainBlock = blockList.get(0);
+           
+           //Sees if mainblock is finished falling then stops falling. Removes block from blockList and updates board with new board
+           
+           
+           
+           //Determines if block is going down
+           if(ticks%fallSpeed==0||action==DOWN){
+             mainBlock.moveDown();
+             
+             //If the player wants to get down
              if(action==DOWN){
                score+=1;
              }
            }
+           
+           //Checks if any row has been cleared and changes score, level, and rowsCleared appropriately
            int tempRows = rowsCleared;
            boolean fullRow;
            for(int i = 0; i<height; i++){
@@ -63,6 +88,10 @@ public class TetrisController{
               if(fullRow){
                 score+=100;
                 rowsCleared++;
+                fallSpeed-=10;
+                if(fallSpeed<50){
+                  fallSpeed = 50;
+                }
                 for(int j = i; j<height; j++){
                   board[j] = board[j+1].clone();
                 }
@@ -72,9 +101,19 @@ public class TetrisController{
                 level++;
               }
            }
-           
-           switch tw.getUserAction(){
-             //Add cases here. Not sure how to move blocks with this though.
+           //Controls the actions
+            switch action{
+              case UP:
+                mainBlock.rotate();
+                break; 
+              case LEFT:
+                mainBlock.moveLeft();
+                break;
+              case RIGHT:
+                mainBlock.moveRight();
+                break;
+              case default:
+                break;
            }
            
            ticks++;
