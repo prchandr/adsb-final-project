f1b7a11811207b1725bae80e43a4e8587cb2ab6c
prchandr <happyprems@gmail.com>
2016-06-13 10:20:55 -0400

    Update Tetris.java

--- Tetris.java
+++ Tetris.java
@@ -1,9 +1,9 @@
 public class Tetris{
-  private Board board = new Board();
-  private TetrisView view = new TetrisView(board);
-  private TetrisController controller = new TetrisController(board, view);;
-  
-  public static void main(String[] args){
-    controller.runGame();
-  }
+	private static Board board = new Board();
+	private static TetrisView view = new TetrisView(board);
+	private static TetrisController controller = new TetrisController(board, view);
+
+	public static void main(String[] args){
+		controller.runGame();
+	}
 }
