83df9d3366aa05a89be16748139cb2f9b81e99bd
brennanwiner <brennan.winer@gmail.com>
2016-06-13 22:20:23 -0400

    needed to be static

--- Tetris.java
+++ Tetris.java
@@ -1,7 +1,9 @@
+package net.mrpaul.ads.QM200.may;
+
 public class Tetris{
-  private Board board = new Board();
-  private TetrisView view = new TetrisView(board);
-  private TetrisController controller = new TetrisController(board, view);;
+  private static Board board = new Board();
+  private static TetrisView view = new TetrisView(board);
+  private static TetrisController controller = new TetrisController(board, view);
   
   public static void main(String[] args){
     controller.runGame();
