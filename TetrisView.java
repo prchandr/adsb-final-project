851fb257fceb8ae1cef9312baa645664119877e6
brennanwiner <brennan.winer@gmail.com>
2016-06-13 21:09:45 -0400

    fixed of all syntax errors (but this doesn't mean it works)

--- TetrisView.java
+++ TetrisView.java
@@ -1,12 +1,19 @@
-import javafx.application.Application;
 
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
 //DELETE THIS AFTER
 import java.util.Scanner;
 
 public class TetrisView extends Application{
 	private Board board;
 	private BlockFactory bf;
-	private ArrayList<Block> blockList = new ArrayList<Block>();
+	private Block[][] blockList = new Block[][]{};
+
 	
 	public TetrisView(Board b){
 		board = b;
@@ -16,26 +23,31 @@ public class TetrisView extends Application{
 		board = b;
 	}
 	
+	public enum UserAction {
+		UP, LEFT, RIGHT, DOWN, RESET, PAUSE, EXIT, INVALID
+	}
+	
 	//Gets the KeyBoard Event and returns the correct enum value
 	public UserAction getUserAction(){
 		/*
 		REPLACE THIS WITH JAVAFX AND KEYBOARD EVENTS LATER ON
 		*/
-		Scanner in = new Scanner();
-		System.out.print("Enter [W]Up, [A]Left, [S]Down, [D]Right, [ESC]Quit: ")
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter [W]Up, [A]Left, [S]Down, [D]Right, [ESC]Quit: ");
 		String result = in.nextLine();
+		UserAction action = UserAction.INVALID;
 		
-		switch result{
+		switch(result){
 			case "W":
-				return UP;
+				return UserAction.UP;
 			case "A":
-				return LEFT;
+				return UserAction.LEFT;
 			case "S":
-				return DOWN;
+				return UserAction.DOWN;
 			case "D":
-				return RIGHT;
+				return UserAction.RIGHT;
 			default:
-				return INVALID;
+				return UserAction.INVALID;
 		}
 	}
 	
@@ -53,21 +65,15 @@ public class TetrisView extends Application{
 		primaryStage.setMinHeight(400);
 		Scanner s = new Scanner(System.in);
 		System.out.println("Board Width?");
-		int width = Scanner.next();
+		int width = Integer.parseInt(s.next());
 		System.out.println("Board Length?");
-		int length = Scanner.next();
+		int length = Integer.parseInt(s.next());
 		TilePane tile = new TilePane(Orientation.VERTICAL);
     		tile.setTileAlignment(Pos.CENTER_LEFT);
     		if(width > 0 && width < 100){
     		tile.setPrefRows(width);}
     		if(length > 0 && length < 100){
-    		tile.setPrefColumns(length);}
-		for (int i = 0; i < 50; i++) {
-        	tile.getChildren().add(bf);}
-    }
-		
-		
-		
+    		tile.setPrefColumns(length);}  		
 		primaryStage.show();
 	}
 	
@@ -75,4 +81,5 @@ public class TetrisView extends Application{
 	public static void main(String[] args){
 		launch(args);
 	}
+
 }
